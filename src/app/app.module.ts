267aa64ba436eb82c0f1549cdce5275669739072
Aaron McCarthy <amccarthy9904@gmail.com>
2021-03-29 18:55:25 -0700

    initial commit: navbar and footer - contact us, about, home, pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    ContactUsComponent,
+    HomeComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
